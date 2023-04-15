d51731e438a851a02af4c30c06d470b31db8d0dc
nklau <63122247+nklau@users.noreply.github.com>
2023-04-15 14:32:00 -0700

    tag button
    Co-authored-by: Abe <abemo@users.noreply.github.com>

---
+++ src/components/TagButton.tsx
@@ -0,0 +1,26 @@
+import { tagColors } from '../data/tags'
+
+type TagButtonProps = {
+  name: string,
+  color?: string,
+  textColor?: string,
+}
+
+const TagButton = ({ name, color = 'white', textColor = 'black' }: TagButtonProps) => {
+  const buttonStyle = {
+    backgroundColor: color,
+    color: textColor,
+    border: 'none',
+    borderRadius: 0,
+  }
+
+  return (
+    <button style={buttonStyle}>#{name}</button>
+  )
+}
+
+const renderTagButton = (tag: keyof typeof tagColors) => {
+  return <TagButton name={tag} color={tagColors[tag][0]} textColor={tagColors[tag][1]}/>
+}
+
+export default renderTagButton
