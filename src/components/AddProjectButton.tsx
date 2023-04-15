c0a972ce1f79b2c8c7b1cb930652c9acc10dffe1
nklau <63122247+nklau@users.noreply.github.com>
2023-04-15 15:35:57 -0700

    remove unused style

--- src/components/AddProjectButton.tsx
+++ src/components/AddProjectButton.tsx
@@ -1,15 +1,6 @@
 import '../css/addProjectButton.css'
 
 const AddProjectButton = () => {
-  const style = {
-    border: '2px solid #73a2f5',
-    backgroundColor: 'white',
-    color: '#73a2f5',
-    height: '25px',
-    width: '25px',
-    alignSelf: 'center',
-    borderRadius: '20%',
-  }
   return (
     <button className="add-project-button"
       onClick={() => {
